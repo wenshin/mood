9106592045ad7159ce4795cc49ce0946083b0b7d
yanwx <yanwx@knownsec.com>
2014-11-16 23:17:20 +0800

    attribute enhance

--- src/moattr.js
+++ src/moattr.js
@@ -9,11 +9,21 @@ var getRender = function(t) {
   return _render[t] || tmpl(t);
 };
 
+// will not escape the html tags
 moAttrs.moHtml = {
   getHook: function(attr) {
     return function(scope) {
       // this is the element
       this.innerHTML = getRender(attr)(scope);
+    };
+  }
+};
+
+// Will escape the html tags
+moAttrs.moText = {
+  getHook: function(attr) {
+    return function(scope) {
+      this.innerHTML = getRender(attr)(scope, false);
     };
   }
 };
