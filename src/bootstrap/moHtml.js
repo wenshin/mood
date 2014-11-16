93dbb7f3e9991c861d9c020b15650fb3966ce574
yanwx <yanwx@knownsec.com>
2014-11-16 18:10:06 +0800

    确定了有问题的自动刷新DOM

--- src/bootstrap/moHtml.js
+++ src/bootstrap/moHtml.js
@@ -1,14 +1,3 @@
-'use strict';
-
-var init = function(scope, elems) {
-  elems.forEach(function(elem) {
-    var curHtml = elem.innerHTML;
-    elem.innerHTML = undefined;
-    Object.defineProperty(elem, 'innerHTML', {
-      set: function(value) {
-        elem.__innerHTML = value.
-      },
-      get: function() {}
-    });
-  });
+window.moAttr.moHtml = function(attr, scope) {
+  this.innerHTML = window.tmpl(attr, scope);
 };
