72440603a764f5b1b6ee67edbe2a520b45c2bbe7
yanwx <yanwx@knownsec.com>
2014-11-16 23:16:56 +0800

    xss safe add to tmpl; mo-text attribute

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,18 +1,28 @@
 'use strict';
 
+(function() {
+  String.prototype.xssSafe = function() {
+    var str = String(this);
+    str = str.replace(/&/g, '&amp;')
+             .replace(/"/g, '&quot;')
+             .replace(/'/g, '&#039;')
+             .replace(/:/g, '&#058;')
+             .replace(/\\/g, '&#092;')
+             .replace(/\//g, '&#047;')
+             .replace(/>/g, '&gt;')
+             .replace(/</g, '&lt;');
+    return str;
+  };
+})();
+
 var Util = require('./utils/util').Util;
 var moAttrs = require('./moattr').moAttrs;
 
 var ATTR_PREFIX = 'mo';
 var SCOPE = 'Scope';
-var attrMetas = ['Html'];
 
 var moAttr = function(meta) {
   return [ATTR_PREFIX, '-', meta.toLowerCase()].join('');
-};
-
-var moModule = function(meta) {
-  return ATTR_PREFIX + meta;
 };
 
 var moAttrSelector = function(meta, value) {
@@ -22,17 +32,21 @@ var moAttrSelector = function(meta, value) {
 
 var getHooksOfScope = function(scopeElem) {
   var hooks = [];
-  Util.each(attrMetas, function(meta) {
+  for ( var p in moAttrs ) {
+    if ( !moAttrs.hasOwnProperty(p) ) { continue; }
+    if ( p.indexOf(ATTR_PREFIX) !== 0 ) { continue; }
+
+    var meta = p.slice(2).toLowerCase();
     var elems = scopeElem.querySelectorAll(moAttrSelector(meta));
     Util.each(elems, function(elem) {
       var attr = elem.getAttribute(moAttr(meta));
-      var hook = moAttrs[moModule(meta)].getHook(attr);
+      var hook = moAttrs[p].getHook(attr);
       hooks.push(function(scope) {
         hook.call(elem, scope);
       });
       elem.removeAttribute(moAttr(meta));
     });
-  });
+  }
   return hooks;
 };
 
