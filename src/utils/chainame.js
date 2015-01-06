e5ec878ddb3dec9de8bb47f7de3cb38189dfce9e
yanwx <yanwx@knownsec.com>
2015-01-06 11:34:59 +0800

    添加chainName 转换成对象方法

--- src/utils/chainame.js
+++ src/utils/chainame.js
@@ -11,6 +11,23 @@ var ChainName = {
   },
   first: function(chain) {
     return this.resolve(chain)[0];
+  },
+  name2Obj: function(chain) {
+    var obj = {}, superObj;
+    chain = this.resolve(chain);
+    if ( chain.length === 1 ) {
+      obj[chain[0]] = null;
+    } else {
+      chain.reduce(function(pre, cur, index) {
+        var tmp = {};
+        tmp[cur] = null;
+        if ( index === 1 ) { obj[pre] = tmp; }// 第一次调用
+        else { superObj[pre] = tmp; }
+        superObj = tmp;
+        return cur;
+      });
+    }
+    return obj;
   }
 };
 
---
+++ test/unit/test_chainname.js
@@ -0,0 +1,24 @@
+/* global describe, it */
+
+'use strict';
+
+var assert = require('chai').assert;
+var ChainName = require('../../dist/utils/chainame').ChainName;
+
+
+describe('ChainName', function() {
+
+  describe('#name2Obj', function() {
+
+    it('should return one layer object with not chain name', function() {
+      var obj = ChainName.name2Obj('me');
+      assert.deepEqual(obj, {me: null});
+    });
+
+    it('should return embeded object with chain name', function() {
+      var obj = ChainName.name2Obj('me.abc.c');
+      assert.deepEqual(obj, {me: {abc: {c: null}}});
+    });
+  });
+
+});
