57fa676a74983ed52335917014cfeedfcf812e5c
yanwx <yanwx@knownsec.com>
2015-01-07 15:09:57 +0800

    测试和清理

--- test/app.js
+++ test/app.js
@@ -6,20 +6,7 @@ require.config({
 require( ['mood'], function(mood) {
   'use strict';
 
-  // debug is true will print the info in console
-  mood.Mood.config({ debug: false });
+  // var myScope = mood.Mood.getScope('myscope');
+  // myScope.count = 2;
 
-  var app = new mood.Mood('myapp');
-
-  var myScope = app.getScope('myscope');
-
-  myScope.defineControllers(function(scope) {
-    var i = 0;
-    setInterval(function() {
-      scope.test = i;
-      scope.name = ', <h1>123</h1><img src="test" onerror="console.log(\'xss danger\')">' + i;
-      // scope.name = ', wenshin' + i;
-      i++;
-    }, 1000);
-  });
 });
