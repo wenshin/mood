aa7f09cec521224e44632dd1205bdb86ff978897
yanwx <yanwx@knownsec.com>
2015-01-07 21:43:17 +0800

    添加formatFloat

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -3,5 +3,10 @@
 exports.Helpers = {
   defaults: function(src, defaultValue) {
     return src || defaultValue;
+  },
+  floatFormat: function(num, digit) {
+    var _num = parseFloat(num);
+    var v = Math.pow(10, digit);
+    return _num ? Math.round( _num * v ) / v : num;
   }
 };
