712e13d9e64d83f315cbd56573b02b0810c4cc76
yanwx <yanwx@knownsec.com>
2015-01-09 13:26:25 +0800

    丰富测试

--- test/app.js
+++ test/app.js
@@ -12,11 +12,22 @@ require( ['mood'], function(mood) {
 
   // 设置Scope的初始值
   mood.Mood.bootstrap({
-    'myscope': { show: true, count: 0 }
+    'myscope': { show: true, count: 0 },
+    'myscope1': { price: 0, buyPrice: 0, referPrice: 0 },
   });
 
-  var myScope1 = mood.Mood.getScope('myscope1');
-  myScope1.helper('vibrate', function(price, preferPrice, rate) {
-    return this.floatFormat(+price + (price - preferPrice) * rate, 2);
-  });
+  var myScope1 = mood.Mood.getScope('myscope1'),
+      triggerProps = ['price', 'buyPrice', 'referPrice'];
+
+  myScope1.helper('vibrate', function(rate) {
+    var price = this.toFloat(this.price) || 0, referPrice = this.toFloat(this.referPrice);
+    return this.floatFormat(price + (price - referPrice) * rate, 2);
+  }, triggerProps);
+
+  myScope1.helper('earn', function(rate) {
+    var vPrice = this.vibrate(rate), buyPrice = this.toFloat(this.buyPrice);
+    if ( !buyPrice ) { return 0; }
+    return this.floatFormat((vPrice - buyPrice) / buyPrice * 100, 2);
+  }, triggerProps);
+
 });
